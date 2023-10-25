6373b7bdd14bffa04077a41927fd236e4b13e770
ALFomas <131364797+ALFomas@users.noreply.github.com>
2023-10-25 14:26:24 +0300

    Update custom_filters.py
    finished writing

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -6,6 +6,7 @@ register = template.Library()
 
 @register.filter(name='censor')
 def censor(value):
+    """ * maker censor """
     unwanted_words = [ 'хрен']
     for word in unwanted_words:
         value = value.replace(word, '*' * len(word))
